c7304851981de27d4948625f49eadd2986b717a1
Daniel Martelly <dmartelly@pocketgems.com>
2015-04-26 15:01:03 -0400

    Added some kind of stop function for text to speech

--- src/tts/TTS.java
+++ src/tts/TTS.java
@@ -20,6 +20,15 @@ public class TTS {
 		voice.speak(str);
 	}
 	
+	// not thread safe. Seems like a dumb way to do it but the
+	// only way I could find at the moment.
+	// could be a cause of problems
+	public void stop() {
+		voice.deallocate();
+		voice = voiceManager.getVoice("kevin16");
+		voice.allocate();
+	}
+	
 	public boolean close() {
 		if (voice != null) {
 			voice.deallocate();
